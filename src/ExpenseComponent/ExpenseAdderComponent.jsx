2a55d4efc699eceb9251ba05eb3a8b9cd1fe9ca1
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2022-07-12 11:44:37 +0530

    2 files modified

--- src/ExpenseComponent/ExpenseAdderComponent.jsx
+++ src/ExpenseComponent/ExpenseAdderComponent.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import './ExpenseStyle.css';
 
 export function ExpenseAdderComponent() {
-  return <h2>Expense Adder</h2>;
+  return (
+    <div class="card-body">
+      <div className="row">
+        <div className="col-md-6">
+          <input type="text" className="form-control" placeholder="Title" />
+        </div>
+        <div className="col-md-6">
+          <input type="text" className="form-control" placeholder="Amount" />
+        </div>
+      </div>
+      <br />
+      <div className="row mt">
+        <div className="col-md-12">
+          <input type="date" className="form-control" placeholder="Date" />
+        </div>
+      </div>
+      <br />
+      <div className="row mt">
+        <div className="col-md-6">
+          <button className="btn btn-primary btn-sm"> Add expense</button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/ExpenseComponent/ExpenseListComponent.jsx
+++ src/ExpenseComponent/ExpenseListComponent.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 
 export function ExpenseListComponent(props) {
   return (
-    <div className="card">
-      <li className="list-group-item">
-        <span className="badge bg-secondary ml">
-          {props.expenseData.date.toLocaleDateString()}
-        </span>
-        <span className="badge bg-primary ml">{props.expenseData.title}</span>
-        <span className="badge bg-success ml">{props.expenseData.amount}</span>
-        <span className="ml">
-          <button className="btn btn-info btn-sm">Action</button>
-        </span>
-      </li>
-    </div>
+    <li class="list-group-item d-flex justify-content-between align-items-start">
+      <div class="ms-2 me-auto">
+        <div class="fw-bold">{props.expenseData.title}</div>
+        {props.expenseData.date.toLocaleDateString()}
+      </div>
+      <span class="badge bg-primary rounded-pill">
+        {props.expenseData.amount}
+      </span>
+    </li>
   );
 }
